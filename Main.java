d3598d2a73b919cfe745a7ee7bf5f0aa4032322b
Jonathan-Keefe <71659470+Jonathan-Keefe@users.noreply.github.com>
2020-09-25 11:01:53 -0400

    Nergigante
    Yes

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("On the Outside I Skrt Skrt but on the inside I Hurt Hurt");
+    System.out.println("On the Outside I Skrt Skrt but on the inside I Hurt Hurt!");
   }
 }
